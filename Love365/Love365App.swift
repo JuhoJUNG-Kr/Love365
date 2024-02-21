0c7d87545fb8c94bbc10d71deeea59ecb3f9f8e6
Juho <jooho293@gmail.com>
2024-02-21 15:51:35 +0100

    Initial Commit

---
+++ Love365/Love365App.swift
@@ -0,0 +1,17 @@
+//
+//  Love365App.swift
+//  Love365
+//
+//  Created by 정주호 on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Love365App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
